2d5e2024366bec50664654d1de83db3319c0edf1
Michael Feurstein <michaelfeurstein@.wu.ac.at>
2025-04-18 12:29:00 +0200

    using nx example

--- simplynx/printVersion.tcl
+++ simplynx/printVersion.tcl
@@ -1,1 +1,17 @@
-puts "printVersion: hello docker world tcl: '$tcl_version'"
+# reference example: https://next-scripting.org/xowiki/file/docs/nx/examples/per-object-mixins.html?html-content=1
+
+nx::Class create BaseClass {
+  :public method speak {} {
+    puts "Hello from BC."
+  }
+}
+
+nx::Class create DerivedClass -superclass BaseClass {
+  :public method speak {} {
+    next
+    puts "Hello from DC."
+  }
+}
+
+DerivedClass create o1
+o1 speak
